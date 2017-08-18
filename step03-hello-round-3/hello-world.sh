56cc53938f295370be713c2e0d1d40926b47602f
Claude Falguiere <cfalguiere@gmail.com>
2017-08-18 19:38:05 +0200

    transfer

--- step03-hello-round-3/hello-world.sh
+++ step03-hello-round-3/hello-world.sh
@@ -4,8 +4,11 @@ name=$1
 #[[ -z $name ]] && { echo "No name provided. Name is mandatory!"; exit 1; }
 
 errors=()
-[[ -z $name ]] && errors+="No name provided. Name is mandatory!"
-[[ $# -ne 1 ]] && errors+="Usage: $0 <name>"
+#[[ -z $name ]] && errors+="No name provided. Name is mandatory!"
+[[ -z $name ]] && errors+=("No name provided. Name is mandatory!")
+#[[ $# -ne 1 ]] && errors+="Usage: $0 <name>"
+[[ $# -ne 1 ]] && errors+=("Usage: $0 <name>")
+#[[ -z ${errors[@]} ]] || { for i in ${errors[@]}; do echo "$i"; done; exit 1; }
 [[ -z ${errors[@]} ]] || { for i in "${errors[@]}"; do echo $i; done; exit 1; }
 
 echo "Hello $name!"
