9f74e28ddb18791d3f50cfa1efd1008d5c39f47e
Claude Falguiere <cfalguiere@gmail.com>
2017-08-18 19:20:16 +0200

    transfer

--- step03-hello-round-3/hello-world.sh
+++ step03-hello-round-3/hello-world.sh
@@ -6,7 +6,6 @@ name=$1
 errors=()
 [[ -z $name ]] && errors+="No name provided. Name is mandatory!"
 [[ $# -ne 1 ]] && errors+="Usage: $0 <name>"
-echo "errors=${errors[@]}"
-[[ -z ${errors[@]} ]] || { echo "${errors[@]}"; exit 1; }
+[[ -z ${errors[@]} ]] || { for i in ${errors[@]}; do echo $i; done; exit 1; }
 
 echo "Hello $name!"
