c6dd327640af81ec9135deebb4a638005f6c2f08
Claude Falguiere <cfalguiere@gmail.com>
2017-08-20 16:31:27 +0200

    transfer

--- step05-hello-round-5/hello-world.sh
+++ step05-hello-round-5/hello-world.sh
@@ -17,7 +17,7 @@ while getopts "vn:" opt; do
       ;;
     n)
       name=$OPTARG
-      [[ verbosity -ge 1 ]] && echo "input parameter name = '""$name""'"
+      [[ $verbosity -ge 1 ]] && echo "input parameter name = '""$name""'"
       ;;
   esac
 done
