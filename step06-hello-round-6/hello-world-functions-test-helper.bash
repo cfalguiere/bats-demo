68bb94915f8f0dfaa457fcba0880da3cda5549fa
Claude Falguiere <cfalguiere@gmail.com>
2017-08-20 21:08:07 +0200

    transfer

--- step06-hello-round-6/hello-world-functions-test-helper.bash
+++ step06-hello-round-6/hello-world-functions-test-helper.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-script_dir=$(readlink -f $0 | xargs dirname)
-source $script_dir/hello-world-functions.sh
+source $BATS_TEST_DIRNAME/hello-world-functions.sh
+#TODO script path
 
 function log_info_test() {
   msg="this is a test" \
