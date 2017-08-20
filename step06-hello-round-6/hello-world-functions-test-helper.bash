320f1a1d8cbe3a1faeab6892ccc9ce9b31e379d4
Claude Falguiere <cfalguiere@gmail.com>
2017-08-20 21:41:05 +0200

    transfer

--- step06-hello-round-6/hello-world-functions-test-helper.bash
+++ step06-hello-round-6/hello-world-functions-test-helper.bash
@@ -2,6 +2,8 @@
 source $BATS_TEST_DIRNAME/hello-world-logs-functions.sh
 #TODO script path
 
-function functions_test_helper() {
-  eval $( $@ )
+function functions_test_helper()
+  CMD=$@
+  echo $CMD
+  eval $( $CMD )
 }
